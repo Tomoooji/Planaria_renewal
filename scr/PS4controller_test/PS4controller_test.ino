cf575950b006962d3f8a7310bd0628b82cdfd390
Tomoooji <168693891+Tomoooji@users.noreply.github.com>
2026-04-09 16:55:11 +0900

    make test file

--- scr/PS4controller_test/PS4controller_test.ino
+++ scr/PS4controller_test/PS4controller_test.ino
@@ -1,6 +1,17 @@
-#include <PS4Controller>
+// test program for PS4 controller //
+
+#include <PS4Controller.h>
 const char MACADDRESS[] = "b0:cb:d8:c0:45:d2";
 
-void setup(){}
+void setup(){
+  Serial.begin(115200);
+  PS4.begin(MACADDRESS);
+}
 
-void loop(){}
+void loop(){
+  if(PS4.isConnected()){
+    Serial.print(PS4.LStickX());
+    Serial.print(",");
+    Serial.println(PS4.LStickY());
+  }
+}

2543ceb4c7d6b30bb62059a5164620b1b437599a
Tomoooji <168693891+Tomoooji@users.noreply.github.com>
2026-05-07 20:34:36 +0900

    make it better

--- src/mitochondria_renewal/Controller_PS4.h
+++ src/mitochondria_renewal/Controller_PS4.h
@@ -4,12 +4,12 @@
 
 class Controller_PS4{
 private:
-  constexpr char[] MACAddress = nullptr;
+  constexpr char* mac_address = nullptr;
   
   struct info{
-    int FRBL_speed; // -255~255
-    int FLBR_speed; // -255~255
-    int turn_speed; // -255(right)~255(left) :ラジアンと同じ
+    float angle; // radian
+    int dist; // 0~255 ?
+    int turn; // -255(right)~255(left) :ラジアンと同じ
   } data;
 
   struct gain{
@@ -17,32 +17,34 @@ private:
     int ignore_limit_triger;
     //int ignore_time_button;
   } config;
+  
+  int _filter(int valX, int valY){
+    return sp(valX)+sp(valY) > sq(this->config.ignore_range_stick)? sqrt(sp(valX)+sp(valY)): 0;
+  }
 
+  int _filter(int val){
+    return (val > this->config.ignore_limit_triger)? val: 0;
+  }
+  
 public:
-  Controller_PS4(const char mac):MACAddress(mac){}
+  Controller_PS4(const char mac):mac_address(mac){}
   
   bool begin(){
-    return PS4.begin(this->MACAddress);
-  }
-
-  bool stickFilter(int valX, int valY){
-    return sp(valX)+sp(valY) > sq(this->config.ignore_range_stick);
-  }
-
-  int trigerFilter(int val){
-    return (val > this->config.ignore_limit_triger)? val: 0;
+    return PS4.begin(this->mac_address);
   }
   
   bool update(){
     if(PS4.isConnected()){
-      this->data.FRBL_speed = stickFilter(PS4.LStickX(),PS4.LStickY()) ? PS4.LStickX()+PS4.LStickY() : 0;
-      this->data.FLBR_speed = stickFilter(PS4.LStickX(),PS4.LStickY()) ? -PS4.LStickX()+PS4.LStickY() : 0;
-      this->data.turn_speed = trigerFilter(PS4.L2) - trigerFilter(PS4.R2);
+      this->data.angle = atan2(PS4.LStickY(),PS4.LStickX());
+      this->data.dist = _filter(PS4.LStickY(),PS4.LStickX());
+      this->data.turn = _filter(PS4.L2()) - _filter(PS4.R2());
       return true;
     }
-    else{
-      return false;
-    }
+    return false;
+  }
+
+  const info get_data(){
+    return &this->data;
   }
 
 };
--- src/mitochondria_renewal/MecanumDriver.h
+++ src/mitochondria_renewal/MecanumDriver.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <Arduino.h>
-#include <AnalogMotorDriver.h>
+#include "AnalogMotorDriver.h"
 
 class Mecanum{
 private:
@@ -16,9 +16,19 @@ private:
   } config;
 
 public:
-  Mecanum();
+  Mecanum(){}
  
-  void begin(const uint8_t pin[]);
+  void begin(const uint8_t pin_FR[], const uint8_t pin_BL[], const uint8_t pin_BR[], const uint8_t pin_FR[]){
+     this->motors[FRONTLEFT].attach(pin_FR);
+      this->motors[BACKLEFT].attach(pin_BL);
+     this->motors[BACKRIGHT].attach(pin_BR);
+    this->motors[FRONTRIGHT].attach(pin_FR);
+  }
 
-  void move(int FR_BL, int FL_BR, int turn);
+  void move(float direction, int speed_line, int speed_turn){
+     this->motors[FRONTLEFT].move((speed_line *cos(0.25*PI +direction) +speed_turn)*(this->config.reversed[FRONTLEFT] ?-1:1));
+      this->motors[BACKLEFT].move((speed_line *sin(0.25*PI +direction) +speed_turn)*(this->config.reversed[BACKLEFT]  ?-1:1));
+     this->motors[BACKRIGHT].move((speed_line *cos(0.25*PI +direction) -speed_turn)*(this->config.reversed[BACKRIGHT] ?-1:1));
+    this->motors[FRONTRIGHT].move((speed_line *sin(0.25*PI +direction) -speed_turn)*(this->config.reversed[FRONTRIGHT]?-1:1));
+  }
 };

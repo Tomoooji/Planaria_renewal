d519a1507a31c95785fe3e1c9480507dead11c79
Tomoooji <168693891+Tomoooji@users.noreply.github.com>
2026-05-20 17:57:02 +0900

    ちょっとだけブラッシュアップ

--- planaria_renewal/planaria_renewal.ino
+++ planaria_renewal/planaria_renewal.ino
@@ -8,16 +8,18 @@ constexpr uint8_t FLpin[] = {27,14};
 constexpr uint8_t BLpin[] = {25,26};
 constexpr uint8_t BRpin[] = {32,33};
 constexpr uint8_t FRpin[] = {17,16};
+int leg_max[] = {170,170,170,170};
 Mecanum leg;
-//leg.get_config().reversed[Mecanum::FRONTLEFT]=true;
 
 void setup(){
-  dualshock4.get_config().ignore_range_stick=50;
-  dualshock4.get_config().ignore_limit_triger=50;
-  leg.get_config().max_speed[0]=170;
-  leg.get_config().max_speed[1]=170;
-  leg.get_config().max_speed[2]=170;
-  leg.get_config().max_speed[3]=170;
+  dualshock4.get_config().ignore_range_stick = 50;
+  dualshock4.get_config().ignore_limit_triger = 50;
+
+  *leg.get_config().max_speed = *leg_max;
+  //leg.get_config().max_speed[0] = 170;
+  //leg.get_config().max_speed[1] = 170;
+  //leg.get_config().max_speed[2] = 170;
+  //leg.get_config().max_speed[3] = 170;
 
   if(!dualshock4.begin()) return;
   leg.begin(FLpin,BLpin,BRpin,FRpin);
@@ -30,7 +32,6 @@ void loop(){
         dualshock4.get_input().dist,
         dualshock4.get_input().turn
     );
-    //leg.move();
   }
   else{
     leg.update(0, 0, 0);

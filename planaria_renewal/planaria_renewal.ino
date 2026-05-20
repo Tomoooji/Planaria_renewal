eeee05995a186e867ddb75ffec9bdcdb40264c08
Tomoooji <168693891+Tomoooji@users.noreply.github.com>
2026-05-20 10:05:40 +0900

    set pin & mac

--- planaria_renewal/planaria_renewal.ino
+++ planaria_renewal/planaria_renewal.ino
@@ -1,13 +1,13 @@
 #include "src/Controller_PS4.h"
 #include "src/MecanumDriver.h"
 
-constexpr char mac[] = "00:00:00:00:00:00";
+constexpr char mac[] = "00:00:00:00:00:01";
 Controller_PS4 ds4(mac);
 
-constexpr uint8_t FLpin[2] = {1,2};
-constexpr uint8_t BLpin[2] = {1,2};
-constexpr uint8_t BRpin[2] = {1,2};
-constexpr uint8_t FRpin[2] = {1,2};
+constexpr uint8_t FLpin[] = {14,27};
+constexpr uint8_t BLpin[] = {26,25};
+constexpr uint8_t BRpin[] = {33,32};
+constexpr uint8_t FRpin[] = {16,17};
 Mecanum leg;
 
 void setup(){
